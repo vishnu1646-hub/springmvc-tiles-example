73aa13136dd3349b836cc2a2316f2f402ec1055b
maddy <maddy@LAPTOP-LFQHQP4A>
2021-04-28 12:06:00 +0530

    spring mvc tiles example

---
+++ SpringMVCTiles/src/main/java/com/vishnu/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.vishnu.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/hello")
+	public String helloWorld(Model m) {
+		String message = "Hello World, Spring MVC @ Javatpoint";
+		m.addAttribute("message", message);
+		return "hello";
+	}
+}
---
+++ SpringMVCTiles/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Spring MVC Example</title>
+</head>
+<body>
+	<a href="hello.html">Hello Spring</a> |
+	<a href="contact.html">Contact</a>
+</body>
+</html>
